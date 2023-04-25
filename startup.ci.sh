1d22e3bbdec91ad3612f06c84690989635130556
ilmerainen <rborukha@gmail.com>
2023-04-26 00:46:44 +0300

    ci: add build-and-deploy workflow

--- startup.ci.sh
+++ startup.ci.sh
@@ -4,8 +4,7 @@ set -e
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
 npm run seed:run
-npm run start:prod > /dev/null 2>&1 &
-/opt/wait-for-it.sh maildev:1080
-/opt/wait-for-it.sh localhost:3000
-npm run lint
-npm run test:e2e -- --runInBand
+npm run start:prod > /dev/null 2>&1
+#/opt/wait-for-it.sh localhost:3001
+#npm run lint
+#npm run test:e2e -- --runInBand
